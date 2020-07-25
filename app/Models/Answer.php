bb0dd92a13138f92c18efa16add2966e7fd4607e
qiuyu <530004000@qq.com>
2020-07-25 20:02:35 +0800

    ide-helper

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -4,6 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Answer
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $question_id
+ * @property string $content
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Question $question
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Answer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Answer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Answer query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Answer whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Answer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Answer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Answer whereQuestionId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Answer whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Answer whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Answer extends Model
 {
     protected $guarded = ['id'];
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,6 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Question
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $title
+ * @property string $content
+ * @property string|null $published_at
+ * @property int|null $best_answer_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Answer[] $answers
+ * @property-read int|null $answers_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Question newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Question newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Question published()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Question query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Question whereBestAnswerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Question whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Question whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Question whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Question wherePublishedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Question whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Question whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Question whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Question extends Model
 {
     protected $guarded = ['id'];
